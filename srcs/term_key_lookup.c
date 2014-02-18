210d5197989e37996ec2fabe2d79223dd4e958ac
Christophe Mehay <cmehay@student.42.fr>
2014-02-18 13:53:40 +0100

    norme

--- srcs/term_key_lookup.c
+++ srcs/term_key_lookup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   term_key_lookup.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbethoua <sbethoua@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cmehay <cmehay@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/03 17:49:08 by sbethoua          #+#    #+#             */
-/*   Updated: 2014/02/03 20:49:55 by sbethoua         ###   ########.fr       */
+/*   Updated: 2014/02/18 13:47:11 by cmehay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,74 @@
 
 static t_key_func_assoc	func_assoc[17] =
 {
-	{KEY_ARROW_UP, ms_key_is_arrow_up},
-	{KEY_ARROW_DOWN, ms_key_is_arrow_down},
-	{KEY_ARROW_RIGHT, ms_key_is_arrow_right},
-	{KEY_ARROW_LEFT, ms_key_is_arrow_left},
-	{KEY_SHIFT_ARROW_UP, ms_key_is_shift_arrow_up},
-	{KEY_SHIFT_ARROW_DOWN, ms_key_is_shift_arrow_down},
-	{KEY_SHIFT_ARROW_RIGHT, ms_key_is_shift_arrow_right},
-	{KEY_SHIFT_ARROW_LEFT, ms_key_is_shift_arrow_left},
-	{KEY_HOME, ms_key_is_home},
-	{KEY_END, ms_key_is_end},
-	{KEY_CTRL_X, ms_key_is_ctrl_x},
-	{KEY_CTRL_Y, ms_key_is_ctrl_y},
-	{KEY_CTRL_P, ms_key_is_ctrl_p},
-	{KEY_CTRL_V, ms_key_is_ctrl_v},
-	{KEY_CTRL_D, ms_key_is_ctrl_d},
-	{KEY_BACKSPACE, ms_key_is_backspace},
-	{0, NULL}
+	{
+		KEY_ARROW_UP,
+		ms_key_is_arrow_up
+	},
+	{
+		KEY_ARROW_DOWN,
+		ms_key_is_arrow_down
+	},
+	{
+		KEY_ARROW_RIGHT,
+		ms_key_is_arrow_right
+	},
+	{
+		KEY_ARROW_LEFT,
+		ms_key_is_arrow_left
+	},
+	{
+		KEY_SHIFT_ARROW_UP,
+		ms_key_is_shift_arrow_up
+	},
+	{
+		KEY_SHIFT_ARROW_DOWN,
+		ms_key_is_shift_arrow_down
+	},
+	{
+		KEY_SHIFT_ARROW_RIGHT,
+		ms_key_is_shift_arrow_right
+	},
+	{
+		KEY_SHIFT_ARROW_LEFT,
+		ms_key_is_shift_arrow_left
+	},
+	{
+		KEY_HOME,
+		ms_key_is_home
+	},
+	{
+		KEY_END,
+		ms_key_is_end
+	},
+	{
+		KEY_CTRL_X,
+		ms_key_is_ctrl_x
+	},
+	{
+		KEY_CTRL_Y,
+		ms_key_is_ctrl_y
+	},
+	{
+		KEY_CTRL_P,
+		ms_key_is_ctrl_p
+	},
+	{
+		KEY_CTRL_V,
+		ms_key_is_ctrl_v
+	},
+	{
+		KEY_CTRL_D,
+		ms_key_is_ctrl_d
+	},
+	{
+		KEY_BACKSPACE,
+		ms_key_is_backspace
+	},
+	{
+		0,
+		NULL
+	}
 };
 
 int	ms_key_lookup(t_context *ctx, uint64_t key, t_cmd_char **cmd_char)
